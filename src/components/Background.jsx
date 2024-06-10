89cbec6da992058e88e97c57584c92b109078a9d
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-10 23:49:14 +0530

    Font, Background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 function Background() {
   return (
-    <div>
-      
+
+    // We cannot return 2 elements together so we used fragments <> </>
+    <>
+
+    <div className='fixed z-[2] w-screen h-screen'>
+        <div className='mt-7 w-full py-10 flex justify-center text-zinc-400 font-semibold text-xl'>Documents.</div>
+        <h1 className='absolute top-1/2 left-1/2 -translate-x-[52%] -translate-y-[52%] text-[13vw] leading-none tracking-tighter text-zinc-900 font-semibold'>Docs.</h1>
+
     </div>
+       
+    </>
   )
 }
 
